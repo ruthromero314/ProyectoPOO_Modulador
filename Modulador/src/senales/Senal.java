0b6dac5db272ad31a9d13e62ed1f12f14654b427
ruthromero314 <ruth.romero@fiuna.edu.py>
2026-05-19 11:17:48 -0300

    Senal

--- Modulador/src/senales/Senal.java
+++ Modulador/src/senales/Senal.java
@@ -8,6 +8,24 @@ package senales;
  *
  * @author Alumnos
  */
-public class Senal {
-    
+public abstract class Senal {
+
+    protected double[] tiempo;
+    protected double[] valores;
+    protected int N = 500;
+
+    public Senal() {
+        tiempo = new double[N];
+        valores = new double[N];
+        for (int i = 0; i < N; i++) {
+            tiempo[i] = i * 0.01;
+        }
+    }
+
+    public abstract void generar();
+
+    public double[] getValores() {
+        return valores;
+    }
+
 }
